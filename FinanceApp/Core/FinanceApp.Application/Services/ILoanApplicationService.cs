a68cc28463c579a88eb40ba156a01a1f8f96b8a1
Mert Yiğit Doğrusöz <mertdogrusoz2@gmail.com>
2025-05-25 21:12:55 +0300

    Added ILoanApplication Service

---
+++ FinanceApp/Core/FinanceApp.Application/Services/ILoanApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using FinanceApp.Application.DTOs;
+using FinanceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Application.Services
+{
+	public interface ILoanApplicationService
+	{
+		Task<int> CreateBasvuruAsync(LoanApplicationDto dto);
+		Task<LoanApplication> GetBasvuruByIdAsync(int id);
+		Task<IEnumerable<LoanApplication>> GetBasvurularByHesapAsync(string hesapNo);
+		Task ApproveBasvuruAsync(int basvuruId);
+		Task RejectBasvuruAsync(int basvuruId, string reason);
+	}
+}
