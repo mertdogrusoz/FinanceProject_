d548265333f3e636d524d7a646287c712c1b9766
Mert Yiğit Doğrusöz <mertdogrusoz2@gmail.com>
2025-05-25 20:18:04 +0300

    Added Account Entities

---
+++ FinanceApp/Core/FinanceApp.Domain/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Domain.Entities
+{
+	public class Account
+	{
+		public int Id { get; set; }
+		public string AccountNumber { get; set; }
+		public string IBAN { get; set; }
+		public decimal Decimal { get; set; }
+		public string AccountOwner { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public bool IsActive { get; set; }
+
+	}
+}
---
+++ FinanceApp/Core/FinanceApp.Domain/Entities/Moneytransfer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Domain.Entities
+{
+	public class Moneytransfer
+	{
+		public int Id { get; set; }
+		public int SenderAccountId { get; set; }
+		public int ReceiverAccountId { get; set; }
+		public decimal Amount { get; set; }
+		public string Description { get; set; }
+		public DateTime TransferDate { get; set; }
+		public TransferStatus Status { get; set; }
+		public string ReferenceNumber { get; set; }
+
+		public virtual Account SenderAccount { get; set; }
+		public virtual Account ReceiverAccount { get; set; }
+
+	}
+}
