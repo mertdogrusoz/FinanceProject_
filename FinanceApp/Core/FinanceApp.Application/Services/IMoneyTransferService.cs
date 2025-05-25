62e7399150c31cffe0ae2e9189edc5f7ce913be8
Mert Yiğit Doğrusöz <mertdogrusoz2@gmail.com>
2025-05-25 21:01:47 +0300

    Added IMoneyTransferService

---
+++ FinanceApp/Core/FinanceApp.Application/Services/IMoneyTransferService.cs
@@ -0,0 +1,18 @@
+﻿using FinanceApp.Application.DTOs;
+using FinanceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Application.Services
+{
+	public interface IMoneyTransferService
+	{
+		Task<TransferResultDto> TransferYapAsync(MoneyTransferDto dto);
+		Task<IEnumerable<Moneytransfer>> GetTransferHistoryAsync(string accountNumber, DateTime? started = null, DateTime? finished = null);
+		Task<Moneytransfer> GetTransferByReferansAsync(string ReferenceNumber);
+
+	}
+}
